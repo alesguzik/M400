f73fbe0b92f96c5a178257bacbaec73d079d4639
Ales Guzik <me@aguzik.net>
2014-01-22 04:08:56 +0300

    adedd md5sums

---
+++ 1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "nof3arno3xith3r3"|md5sum
+echo "no f3ar no 3xit h3r3"|md5sum
+echo "NOF3ARNO3XITH3R3"|md5sum
+echo "NO F3AR NO 3XIT H3R3"|md5sum
+echo "nofearnoexithere"|md5sum
+echo "no fear no exit here"|md5sum
+echo "NOFEARNOEXITHERE"|md5sum
+echo "NO FEAR NO EXIT HERE"|md5sum
+echo -n "nof3arno3xith3r3"|md5sum
+echo -n "no f3ar no 3xit h3r3"|md5sum
+echo -n "NOF3ARNO3XITH3R3"|md5sum
+echo -n "NO F3AR NO 3XIT H3R3"|md5sum
+echo -n "nofearnoexithere"|md5sum
+echo -n "no fear no exit here"|md5sum
+echo -n "NOFEARNOEXITHERE"|md5sum
+echo -n "NO FEAR NO EXIT HERE"|md5sum
